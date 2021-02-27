8fcf1d84082b18c94a021e4bfaf9e78f7c1041d1
Tisdale511 <tisdalefry@gmail.com>
2021-02-27 12:08:09 -0500

    fixed an assue that allowed for 4 consecutive characters

--- 12romanToInt.js
+++ 12romanToInt.js
@@ -32,7 +32,7 @@ var romanToInt = function(s) {
     let nums ={I:1, V:5, X:10, L:50, C:100, D:500, M:1000}
     let ans = 0, curr = 0, prev = 0
     for(let i = s.length - 1; i >= 0; i -= 1){
-        curr = nums[s[i]]
+        curr = nums[s[i]] 
         curr < prev ? ans -= curr : ans += curr
         prev = curr
     }
