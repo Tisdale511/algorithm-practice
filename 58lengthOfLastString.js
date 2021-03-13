a4e895129abd123cb139176cd8a2b3ee05c536bc
Tisdale511 <tisdalefry@gmail.com>
2021-03-13 15:08:49 -0500

    leet code length of last string algo condensed solution

--- 58lengthOfLastString.js
+++ 58lengthOfLastString.js
@@ -1,15 +1,25 @@
+// var lengthOfLastWord = function(s) {
+//     if(s.length === 0){
+//         return 0
+//     }
+
+//     let sArr = s.split(' ');
+//     for(i = sArr.length - 1; i > -1; i -= 1){
+//         if(sArr[i] === ''){
+//             continue
+//         }else{
+//             return sArr[i].length
+//         }
+//     }
+// };
+
+
 var lengthOfLastWord = function(s) {
     if(s.length === 0){
-        return  0
+        return 0
     }
-    let sArr = s.split(' ');
-    for(i = sArr.length - 1; i > -1; i -= 1){
-        if(sArr[i] === ''){
-            continue
-        }else{
-            return sArr[i].length
-        }
-    }
+    let sArr = s.trim().split(' ');
+    return sArr[sArr.length - 1].length;
 };
 
 console.log(lengthOfLastWord("axfgbdfg  "))
