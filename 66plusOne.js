83459bdea7484e6fc0eacd924d2d773272fa065d
Tisdale511 <tisdalefry@gmail.com>
2021-04-03 14:04:33 -0400

    code optimized

--- 66plusOne.js
+++ 66plusOne.js
@@ -2,7 +2,7 @@ var plusOne = function(digits) {
     let digStr = digits.join('')
     let hugeInt = BigInt(digStr)
     hugeInt ++
-    // or hugeInt += BigInt(1)
+    // or hugeInt += BigInt(1)  
     let plusOneArr = hugeInt.toString().split('')
     return plusOneArr
 };
