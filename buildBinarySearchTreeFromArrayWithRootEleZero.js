ca9c64d33eb4199d2d3428a53a76cea54704d57c
Tisdale511 <tisdalefry@gmail.com>
2021-05-21 13:15:24 -0400

    built function to make a tree from array input

--- buildBinarySearchTreeFromArrayWithRootEleZero.js
+++ buildBinarySearchTreeFromArrayWithRootEleZero.js
@@ -6,4 +6,11 @@ function TreeNode(val, left, right) {
 
 }
 
+const createTree = (array) => {
+    const root = new TreeNode(array[0])
+    for(let i = 1; i < array.length; i += 1){
+        addToTree(root, array[i])
+    }
+    return root
+}
 
