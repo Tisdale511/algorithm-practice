55f5e78d129646056fd57041157c35f4879f9a7f
Tisdale511 <tisdalefry@gmail.com>
2021-06-05 17:34:26 -0400

    logic added

--- 205reverseLinkedList.js
+++ 205reverseLinkedList.js
@@ -3,5 +3,11 @@ var reverseList = function(head) {
     let prev = null;
     let curr = head;
     let next = curr.next;
-
+    while (curr) {
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        if (next) next = next.next;
+      }
+      
   };
